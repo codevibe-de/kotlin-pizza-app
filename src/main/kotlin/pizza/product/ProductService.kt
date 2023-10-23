fb0f85bc7c6f42dbc3b458f5e22b2967d9f097eb
Thomas Auinger <thomas@auinger.de>
2023-10-23 11:30:05 +0200

    ProductService and Test

--- src/main/kotlin/pizza/product/ProductService.kt
+++ src/main/kotlin/pizza/product/ProductService.kt
@@ -5,6 +5,12 @@ import org.springframework.stereotype.Service
 @Service
 class ProductService {
 
+    fun getProduct(productId: String) : Product {
+        // todo
+        return Product()
+    }
+
+
     fun createProduct(product: Product) {
         // todo
     }
