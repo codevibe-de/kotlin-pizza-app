52d754eecbff69d7d92756c2d8ff7a6587f13297
Thomas Auinger <thomas@auinger.de>
2023-10-25 15:02:32 +0200

    order entity

--- src/main/kotlin/pizza/order/Order.kt
+++ src/main/kotlin/pizza/order/Order.kt
@@ -1,12 +1,22 @@
 package pizza.order
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 import pizza.customer.Customer
 import java.time.LocalDateTime
 
+@Entity
+@Table(name = "orders")
 class Order {
 
+    @Id
+    @GeneratedValue
     var id: Long? = null
 
+    @ManyToOne
     var customer: Customer? = null
 
     var totalPrice: Double? = null
